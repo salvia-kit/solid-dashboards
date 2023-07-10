d523276d3f25b84054600616c9336b75f3d5acca
Enoch Ndika <enochbeloved@gmail.com>
2023-07-10 18:40:14 +0100

    add dashboard v2

---
+++ dashboard-v2/src/dashboard/sidebar/icons/AppsIcon.tsx
@@ -0,0 +1,22 @@
+export function AppsIcon() {
+  return (
+    <svg viewBox="0 0 512 512" class="h-3.5 w-3.5">
+      <g fill="currentColor">
+        <path
+          d="M0 0h128v128H0zm0 0M192 0h128v128H192zm0 0M384 0h128v128H384zm0 0M0 192h128v128H0zm0 0"
+          data-original="#bfc9d1"
+        />
+      </g>
+      <path
+        d="M192 192h128v128H192zm0 0"
+        fill="currentColor"
+        data-original="#82b1ff"
+      />
+      <path
+        d="M384 192h128v128H384zm0 0M0 384h128v128H0zm0 0M192 384h128v128H192zm0 0M384 384h128v128H384zm0 0"
+        fill="currentColor"
+        data-original="#bfc9d1"
+      />
+    </svg>
+  );
+}
